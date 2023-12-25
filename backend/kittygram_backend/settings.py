075fa7705799f9ef159de082d40879af0e049e91
Михаил Валюшицкий <michaelkiller97@gmail.com>
2023-12-26 00:18:20 +0300

    text x

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default="prompt")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://kittygramme.gotdns.ch/"]
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "django.contrib.admin",
